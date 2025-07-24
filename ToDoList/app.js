4a2318d738c732188cd63ea690c83d8584438fe0
fnascimentosilva <fabricionsilva26@gmail.com>
2025-07-23 22:17:19 -0400

    #

---
+++ ToDoList/app.js
@@ -0,0 +1,11 @@
+const pedido = document.getElementById("item")
+const btnAdd = document.getElementById("btn-add")
+
+btnAdd.addEventListener('click', (e) => {
+    
+    console.log(pedido.value);
+    
+})
+
+
+
